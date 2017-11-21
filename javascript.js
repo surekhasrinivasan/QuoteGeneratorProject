75fa89cb713c590cfe32d431b2ced3de1764fbd4
surekha <surekhasrao@gmail.com>
2017-11-21 04:05:56 +0000

    Create index.html and javascript.js

---
+++ javascript.js
@@ -0,0 +1,10 @@
+var quotes = [ "Success is most often achieved by those who don't know that failure is inevitable.--Coco Chanel", "Courage is grace under pressure.--Ernest Hemingway",  "If you are not willing to risk the usual, you will have to settle for the ordinary.--Jim Rohn", "Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning.--Albert Einstein",  "All our dreams can come true if we have the courage to pursue them.--Walt Disney", "The people who are crazy enough to think they can change the world are the ones who do.--Steve Jobs"];
+var randomNum;
+function getQuote(){
+    randomNum = Math.floor(Math.random()*quotes.length);
+//console.log(randomNum);
+document.getElementById("displayQuote").innerHTML=quotes[randomNum];
+}
+function tweet(){
+        window.open("https://twitter.com/intent/tweet?text=" + quotes[randomNum]);
+    }
